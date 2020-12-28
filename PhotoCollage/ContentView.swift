31b0aa5192c22f94a0a49a3c7b89a1e326054096
reshetnikd <dmitry.reshetnik@icloud.com>
2020-12-28 16:45:21 +0200

    Initial Commit

---
+++ PhotoCollage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoCollage
+//
+//  Created by Dmitry Reshetnik on 28.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoCollage/PhotoCollageApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCollageApp.swift
+//  PhotoCollage
+//
+//  Created by Dmitry Reshetnik on 28.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCollageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
